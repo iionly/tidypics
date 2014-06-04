35243d0037e496bba83cb3d2748a33205f9ba4a8
Yann <y.herriau@primobox.com>
2014-06-04 15:18:20 +0200

    input manquantes

--- tidypics/views/default/forms/photos/ajax_upload.php
+++ tidypics/views/default/forms/photos/ajax_upload.php
@@ -31,6 +31,9 @@ echo elgg_echo ( 'tidypics:uploader:instructs', array (
 
 <ul id="tidypics-uploader-steps">
 	<li class="mbm">
+	<input type="hidden" name="album_guid" value="<?php echo $album->getGUID(); ?>" /> <input type="hidden" name="batch"
+					value="<?php echo $batch; ?>" /> <input type="hidden" name="tidypics_token" value="<?php echo $tidypics_token; ?>" /> <input type="hidden" name="user_guid"
+					value="<?php echo elgg_get_logged_in_user_guid(); ?>" /> <input type="hidden" name="Elgg" value="<?php echo session_id(); ?>" />
 		<div id="uploader">
 			<p>Your browser doesn't have Flash, Silverlight or HTML5 support.</p>
 		</div>
